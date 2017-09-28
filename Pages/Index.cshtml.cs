0ca36bc6083a5816fda3bbdb11625c4d08ef918d
Micheal Colhoun <micheal@cts-ni.com>
2017-09-28 16:54:30 +0100

    Modification to update TextArea content during OnPost()

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,30 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TextAreaDemo.Pages
 {
+   
     public class IndexModel : PageModel
     {
-        [BindProperty]
-        public string Messages { get; set; }
+       [BindProperty]
+        public string Messages { get; set;}
 
-        public void OnGet()
-        {
-
-        }
+       [BindProperty]
+        public bool SimpleCheckBox { get; set;} = true;
+    
         public PageResult OnPost()
         {
-            if ( ModelState.IsValid)
-            {
-                //RedirectToPage("Index");
-            }
-            
-            Messages += $"OnPost : Updated On Server {Environment.NewLine}" ;               
+            ModelState["Messages"].AttemptedValue = $"AttemptedValue: { SimpleCheckBox.ToText() } ";
+
             return Page();
+            
         }
     }
+
+#region Extenstion Methods
+    public static class MyExtensions
+    {
+        public static string ToText(this bool checkBox)
+        {
+            return (checkBox ? "Checked" : "Not Checked");
+        }
+    }
+#endregion
+
 }
